c7a27aa5abec7240042dc5724b48b9585dca7446
Sandra <hi-sandra@hotmail.com>
2026-06-01 20:07:01 -0600

    arreglo de productos

---
+++ script.js
@@ -0,0 +1,9 @@
+//arreglo
+
+const productos = [
+    {nombre: "Base de maquillaje MAC", precio: 663, categoria: "Belleza" },
+    {nombre: "Tenis Adidas", precio: 1300, categoria: "Deportes" },
+    {nombre: "Consola PlayStation 5", precio: 8900, categoria: "Videojuegos" },
+    {nombre: "Reloj TOUS", precio: 4500, categoria: "Joyería" },
+    {nombre: "Bocina JBL", precio: 3700, categoria: "Audio" },
+];
